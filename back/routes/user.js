b6601646130d25e3e8f8ae9643ca0607be6c4de7
kimyerimweb <kimyerimweb@gmail.com>
2021-12-31 02:54:12 +0900

    feat: create login function without validation

--- back/routes/user.js
+++ back/routes/user.js
@@ -1,16 +1,51 @@
 const express = require('express')
-
 const router = express.Router()
 
-router.post('/logout', (req, res, next) => {
+const {
+  getAuth,
+  GoogleAuthProvider,
+  signInWithCredential,
+} = require('firebase/auth')
+
+router.post('/login', async (req, res, next) => {
+  //로그인 (현재는 구글만)
+  const { id_token } = req.body
+  const auth = getAuth()
+
+  try {
+    //중복로그인 시도는 안하는지 체크
+    const credential = GoogleAuthProvider.credential(id_token)
+    await signInWithCredential(auth, credential)
+    res.status(200).send('ok')
+  } catch (e) {
+    console.error(e)
+    //오류 코드 보내주기
+  }
+})
+
+router.post('/logout', async (req, res, next) => {
+  //로그아웃
+  // await getAuth.signOut()
   res.status(200).send('ok')
 })
 
-router.post('/login', (req, res, next) => {
-  res.status(200).send('ok')
-}) // /user/login
-
 router.post('/', async (req, res, next) => {
+  //회원가입
+  res.status(201).send('ok')
+})
+
+router.get('/', async (req, res, next) => {
+  //유저 정보 읽기
+  res.status(201).send('ok')
+})
+
+router.patch('/', async (req, res, next) => {
+  //유저 정보 수정
+  res.status(201).send('ok')
+})
+
+router.delete('/', async (req, res, next) => {
+  //유저 삭제
   res.status(201).send('ok')
 })
 
