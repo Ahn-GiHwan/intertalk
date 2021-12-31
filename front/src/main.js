9d4223f80750ca01bcec6035934650af6a76c91e
Ahn-Gihwan <ahngh0113@naver.com>
2021-12-31 12:22:19 +0900

    feat: import style.css

--- front/src/main.js
+++ front/src/main.js
@@ -7,6 +7,7 @@ import { Provider } from 'react-redux'
 import store from './store/configueStore'
 
 import 'reset-css'
+import './style/style.css'
 
 const rootEl = document.getElementById('root')
 
